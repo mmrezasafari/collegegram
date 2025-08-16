58b493dd78d8f5016f7811712c1eee52b18405ac
mohammadreza safari <mohammadrezasafari23@gmail.com>
2025-08-16 14:36:08 +0000

    created input components

--- FrontEnd/src/features/auth/Form.tsx
+++ FrontEnd/src/features/auth/Form.tsx
@@ -1,5 +1,11 @@
+// import { LoginForm } from './LoginForm'
+// import { RegisterForm } from './RegisterForm'
+
 export const Form = () => {
   return (
-    <div className="w-full h-full md:w-[500px] md:h-2/3 flex flex-col bg-backgroundLight md:rounded-3xl py-16 max-md:px-6 md:shadow-formShadow"></div>
+    <div className="w-full h-full md:w-[500px] md:h-2/3 flex flex-col justify-center items-center bg-backgroundLight md:rounded-3xl py-16 max-md:px-6 md:shadow-formShadow">
+      {/* <RegisterForm /> */}
+      {/* <LoginForm /> */}
+    </div>
   )
 }
