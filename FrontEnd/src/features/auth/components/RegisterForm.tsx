0ff76f55e47104a7d0e9e86142a3f8aae794f19a
mmreza <mmrezadev@gmail.com>
2025-08-22 20:05:14 +0330

    add :: added register functionality

--- FrontEnd/src/features/auth/components/RegisterForm.tsx
+++ FrontEnd/src/features/auth/components/RegisterForm.tsx
@@ -9,27 +9,76 @@ import { Input } from '@/features/common/components/ui/Input'
 import { useInput } from '@/features/common/hooks/useInput'
 import {
   emailSchema,
+  registerFormSchema,
   passwordSchema,
+  rePasswordSchema,
   userNameSchema,
   validateWithYup,
 } from '@/utils/validation'
+import type { ComponentProps } from 'react'
+import { useRegister } from '../hooks/useAuth'
+import { ValidationError } from 'yup'
 
 export const RegisterForm = () => {
-  const userName = useInput('', (val) => validateWithYup(userNameSchema, val))
-  const email = useInput('', (val) => validateWithYup(emailSchema, val))
-  const password = useInput('', (val) => validateWithYup(passwordSchema, val))
-  const rePassword = useInput('', (val) => {
-    if (!val) {
-      return 'وارد کردن تکرار رمزعبور اجباری است'
-    } else {
-      return val === password.value
-        ? null
-        : 'مقدار تکرار رمز‌عبور باید با رمزعبور برابر باشد'
+  const userName = useInput('userName', '', (val) =>
+    validateWithYup(userNameSchema, val),
+  )
+  const email = useInput('email', '', (val) =>
+    validateWithYup(emailSchema, val),
+  )
+  const password = useInput('password', '', (val) =>
+    validateWithYup(passwordSchema, val),
+  )
+  const rePassword = useInput('rePassword', '', (val) =>
+    validateWithYup(rePasswordSchema, val, {
+      context: { password: password.value },
+    }),
+  )
+  const { registerMutation } = useRegister()
+
+  const onFormSubmit: ComponentProps<'form'>['onSubmit'] = (e) => {
+    e.preventDefault()
+
+    const values = {
+      email: email.value,
+      password: password.value,
+      userName: userName.value,
+      rePassword: rePassword.value,
     }
-  })
+
+    try {
+      // all things goods
+      registerFormSchema.validateSync(values, { abortEarly: false })
+      registerMutation.mutate({
+        username: userName.value,
+        email: email.value,
+        password: password.value,
+      })
+    } catch (err) {
+      if (err instanceof ValidationError) {
+        const errorMap: Record<string, string> = {}
+        // create mapError object
+        err.inner.forEach((e) => {
+          if (e.path) errorMap[e.path] = e.message
+        })
+
+        //set errors
+        if (errorMap.userName) userName.setError(errorMap.userName)
+        if (errorMap.email) email.setError(errorMap.email)
+        if (errorMap.password) password.setError(errorMap.password)
+        if (errorMap.rePassword) rePassword.setError(errorMap.rePassword)
+
+        // focus the first invalid input
+        const firstErrorField = [userName, email, password, rePassword].find(
+          (f) => errorMap[f.name],
+        )
+        firstErrorField?.ref.current?.focus()
+      }
+    }
+  }
 
   return (
-    <div className="w-[335px] flex flex-col gap-8">
+    <form className="w-[335px] flex flex-col gap-8" onSubmit={onFormSubmit}>
       <h3 className="text-sm text-center">
         برای ثبت‌نام کافیه نام کاربری، ایمیل و یک رمز عبور وارد کنید:
       </h3>
@@ -38,11 +87,14 @@ export const RegisterForm = () => {
           <div className="relative w-full max-w-sm">
             <Input
               type="text"
+              name={userName.name}
+              ref={userName.ref}
               placeholder="نام کاربری"
               className="pr-9"
               value={userName.value}
               onChange={userName.onChange}
               onBlur={userName.onBlur}
+              autoComplete="username"
               aria-invalid={userName.error ? true : false}
             />
             <div className="pointer-events-none absolute right-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground">
@@ -60,11 +112,14 @@ export const RegisterForm = () => {
           <div className="relative w-full max-w-sm">
             <Input
               type="email"
+              name={email.name}
+              ref={email.ref}
               placeholder="ایمیل"
               className="pr-9"
               value={email.value}
               onChange={email.onChange}
               onBlur={email.onBlur}
+              autoComplete="on"
               aria-invalid={email.error ? true : false}
             />
             <div className="pointer-events-none absolute right-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground">
@@ -82,11 +137,14 @@ export const RegisterForm = () => {
           <div className="relative w-full max-w-sm">
             <Input
               type="password"
+              name={password.name}
+              ref={password.ref}
               placeholder="رمز عبور"
               className="pr-9"
               value={password.value}
               onChange={password.onChange}
               onBlur={password.onBlur}
+              autoComplete="on"
               aria-invalid={password.error ? true : false}
             />
             <div className="pointer-events-none absolute right-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground">
@@ -104,11 +162,14 @@ export const RegisterForm = () => {
           <div className="relative w-full max-w-sm">
             <Input
               type="password"
+              name={rePassword.name}
+              ref={rePassword.ref}
               placeholder="رمز عبور"
               className="pr-9"
               value={rePassword.value}
               onChange={rePassword.onChange}
               onBlur={rePassword.onBlur}
+              autoComplete="on"
               aria-invalid={rePassword.error ? true : false}
             />
             <div className="pointer-events-none absolute right-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground">
@@ -123,7 +184,9 @@ export const RegisterForm = () => {
           )}
         </div>
       </div>
-      <Button className="self-end">ثبت نام</Button>
-    </div>
+      <Button className="self-end" type="submit" loading={registerMutation.isPending}>
+        ثبت نام
+      </Button>
+    </form>
   )
 }
