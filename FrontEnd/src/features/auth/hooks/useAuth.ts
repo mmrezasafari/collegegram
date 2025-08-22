564c435d2a196ad05b0ed486eaeb2781ce288374
mohammadreza safari <mohammadrezasafari23@gmail.com>
2025-08-22 21:23:07 +0000

    Feature/user login

--- FrontEnd/src/features/auth/hooks/useAuth.ts
+++ FrontEnd/src/features/auth/hooks/useAuth.ts
@@ -1,6 +1,6 @@
 import { notify } from '@/features/common/components/ui/sonner'
 import api from '@/lib/axios'
-import type { IRegister, IRegisterRes } from '@/types/auth'
+import type { ILogin, ILoginRes, IRegister, IRegisterRes } from '@/types/auth'
 import { useMutation, useQueryClient, useQuery } from '@tanstack/react-query'
 import { AxiosError } from 'axios'
 
@@ -10,10 +10,18 @@ async function registerUser(user: IRegister): Promise<IRegisterRes> {
   return res.data
 }
 
+async function loginUser(user: ILogin): Promise<ILoginRes> {
+  const res = await api.post<ILoginRes>('/login', user)
+
+  return res.data
+}
+
+
 export function useRegister() {
   const queryClient = useQueryClient()
 
   const registerMutation = useMutation({
+    mutationKey: ['registerUser'],
     mutationFn: registerUser,
     onSuccess: (data) => {
       queryClient.setQueryData(['registerUser'], data)
@@ -42,4 +50,39 @@ export function useRegister() {
   })
 
   return { registerMutation, registerSuccess }
+}
+
+export function useLogin() {
+  const queryClient = useQueryClient()
+
+  const loginMutation = useMutation({
+    mutationKey: ['loginUser'],
+    mutationFn: loginUser,
+    onSuccess: (data) => {
+      queryClient.setQueryData(['loginUser'], data)
+      notify.success('خوش آمدید', {
+        position: 'top-right',
+        duration: 10000
+      })
+    },
+    onError: (error) => {
+      if (error instanceof AxiosError) {
+        notify.error(error.response?.data.message, {
+          position: 'top-right',
+          duration: 10000,
+        })
+      } else {
+        console.error(error)
+      }
+    },
+  })
+
+  const { data: loginSuccess } = useQuery({
+    queryKey: ['registerUser'],
+    initialData: false,
+    enabled: false,
+    queryFn: async () => false,
+  })
+
+  return { loginMutation, loginSuccess }
 }
