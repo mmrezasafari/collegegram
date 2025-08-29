7f173accab9305725e168afdb39699a4a0c8d131
mmreza <mmrezadev@gmail.com>
2025-08-29 17:00:49 +0330

    modify :: change some css about text size of input

--- FrontEnd/src/features/common/components/ui/input.tsx
+++ FrontEnd/src/features/common/components/ui/input.tsx
@@ -13,7 +13,7 @@ function Input({
       id={id}
       data-slot="input"
       className={cn(
-        'file:text-foreground placeholder:text-geryLight selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-grey flex h-9 w-full min-w-0 rounded-3xl border bg-light px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+        'file:text-foreground placeholder:text-geryLight selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-grey flex h-9 w-full min-w-0 rounded-3xl border bg-light px-3 py-1 shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 text-sm',
         'focus-visible:border-ring focus-visible:ring-ring/50',
         'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-textError',
         className,
