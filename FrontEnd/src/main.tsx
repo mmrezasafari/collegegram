d6b37610f1f5cf4541deb4e5012da72f00b52aff
mmreza <mmrezadev@gmail.com>
2025-08-29 17:05:11 +0330

    add :: added strictmode

--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -1,11 +1,11 @@
-// main.tsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
 import './styles/root.css'
 import './styles/globals.css'
 import App from './app/App.tsx'
 
-// import AppRoutes from './app/routes.tsx'
-
-createRoot(document.getElementById('root') as HTMLElement).render(<App />)
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
