5e8fbc3f83f209e1635b1857540fb12c2257de44
mmreza <mmrezadev@gmail.com>
2025-09-02 20:30:18 +0330

    modify :: added and modify validations
    remove required from schema and concat every where need it

--- FrontEnd/src/utils/validation.ts
+++ FrontEnd/src/utils/validation.ts
@@ -4,39 +4,55 @@ import { ValidationError } from 'yup'
 export const emailSchema = yup
   .string()
   .email('فرمت ایمیل صحیح نمی‌باشد')
-  .required('وارد کردن ایمیل اجباری است')
 
 export const passwordSchema = yup
   .string()
+  .min(8, 'رمز عبور حداقل باید ۸ کاراکتر باشد')
   .matches(/[A-Z]/, 'رمز عبور حداقل حاوی یک حرف بزرگ باشد')
   .matches(/[a-z]/, 'رمز عبور حداقل حاوی یک حرف کوچک باشد')
   .matches(/[^a-zA-Z0-9]/, 'رمز عبور حداقل حاوی یک کاراکتر خاص باشد')
-  .min(8, 'رمز عبور حداقل باید ۸ کاراکتر باشد')
-  .required('وارد کردن رمزعبور اجباری است')
 
 export const userNameSchema = yup
   .string()
   .min(5, 'نام‌کاربری حداقل باید ۵ کاراکتر باشد')
-  .required('وارد کردن نام‌کاربری اجباری است')
 
 export const rePasswordSchema = yup
   .string()
-  .required('وارد کردن رمزعبور اجباری است')
   .oneOf([yup.ref('$password')], 'رمز عبور و تکرار آن باید یکسان باشند')
 
 export const registerFormSchema = yup.object({
-  password: passwordSchema,
-  userName: userNameSchema,
+  password: passwordSchema.concat(yup
+    .string()
+    .required('وارد کردن رمزعبور اجباری است')
+  ),
+  userName: userNameSchema.concat(yup
+    .string()
+    .required('وارد کردن نام‌کاربری اجباری است')
+  ),
   rePassword: yup
     .string()
     .required('وارد کردن تکرار رمزعبور اجباری است')
     .oneOf([yup.ref('password')], 'رمز عبور و تکرار آن باید یکسان باشند'),
-  email: emailSchema,
+  email: emailSchema.concat(yup.string().required('وارد کردن ایمیل اجباری است')),
 })
 
 export const loginFormSchema = yup.object({
-  usernameOrEmail: emailSchema,
+  usernameOrEmail: emailSchema.concat(yup.string().required('وارد کردن ایمیل اجباری است')),
+  password: passwordSchema.concat(yup.string().required('وارد کردن رمزعبور اجباری است')),
+})
+
+export const profileEditInfo = yup.object({
+  firstName: yup.string().required('وارد کردن  نام اجباری است'),
+  lastName: yup.string().required('وارد کردن نام خانوادگی اجباری است'),
+  email: emailSchema.concat(yup.string().required('وارد کردن ایمیل اجباری است')),
+})
+
+export const profileEditPassword = yup.object({
   password: passwordSchema,
+  rePassword: yup
+    .string()
+    .required('وارد کردن تکرار رمزعبور اجباری است')
+    .oneOf([yup.ref('password')], 'رمز عبور و تکرار آن باید یکسان باشند'),
 })
 
 export function validateWithYup<T>(
