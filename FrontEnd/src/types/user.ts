2e46ba27c5154955de7d1c7b8a2155a7f52d129c
mmreza <mmrezadev@gmail.com>
2025-09-05 00:39:59 +0330

    modify :: seperate user from registered user

--- FrontEnd/src/types/user.ts
+++ FrontEnd/src/types/user.ts
@@ -1,14 +1,16 @@
 export interface IRegisteredUser {
   success: boolean
-  data: {
-    id: string
-    username: string
-    email: string
-    firstName: string
-    lastName: string
-    bio: string
-    imagePath: string
-    createdAt: string
-    updatedAt: string
-  }
+  data: IUser
+}
+
+export interface IUser {
+  id: string;
+  username: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  bio: string;
+  imagePath: string;
+  createdAt: string;
+  updatedAt: string;
 }
