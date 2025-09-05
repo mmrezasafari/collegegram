8ca91645b0f477e71df0fcc321343c57d9bc14c7
mmreza <mmrezadev@gmail.com>
2025-09-05 21:20:30 +0330

    lint fixed

--- FrontEnd/src/types/user.ts
+++ FrontEnd/src/types/user.ts
@@ -4,13 +4,13 @@ export interface IRegisteredUser {
 }
 
 export interface IUser {
-  id: string;
-  username: string;
-  email: string;
-  firstName: string;
-  lastName: string;
-  bio: string;
-  imagePath: string;
-  createdAt: string;
-  updatedAt: string;
+  id: string
+  username: string
+  email: string
+  firstName: string
+  lastName: string
+  bio: string
+  imagePath: string
+  createdAt: string
+  updatedAt: string
 }
