94c0db5a2cb5ae8fc440c09217a45d129a916784
daleya <kosar.asadipour@gmail.com>
2025-09-08 15:26:14 +0330

    change .env for production

--- BackEnd/src/main.ts
+++ BackEnd/src/main.ts
@@ -1,8 +1,9 @@
 import { makeApp } from "./api";
 import { AppDataSource } from "./data-source";
 import DotenvFlow from "dotenv-flow";
-DotenvFlow.config()
-
+if (process.env.NODE_ENV !== 'production') {
+  DotenvFlow.config()
+}
 AppDataSource.initialize().then((dataSource) => {
   const app = makeApp(dataSource);
   app.listen(3000, () => console.log(`Listening on Port 3000`))
