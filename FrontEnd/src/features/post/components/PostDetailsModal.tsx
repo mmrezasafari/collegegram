38ae37732e2d91eca1e7cf324cdd62419d62401a
mohammadreza safari <mohammadrezasafari23@gmail.com>
2025-09-12 21:28:51 +0000

    Feature/edit post

--- FrontEnd/src/features/post/components/PostDetailsModal.tsx
+++ FrontEnd/src/features/post/components/PostDetailsModal.tsx
@@ -22,6 +22,9 @@ import 'dayjs/locale/fa'
 import { parseCaption } from '@/utils/textDecoration'
 import { useToggleSavePost } from '@/features/bookmark/hooks/useBookmark'
 import { useToggleLike } from '@/features/like/hooks/useLike'
+import { DialogAndModalWizard } from '@/features/common/components/layout/DialogAndModalWizard'
+import { UploadPostForm } from './UploadPostForm'
+import { Link } from 'react-router-dom'
 
 interface IProp {
   postId: string
@@ -35,9 +38,10 @@ export const PostDetailsModal = ({ postId }: IProp) => {
   const post = data?.data
   const [api, setApi] = useState<CarouselApi>()
   const [current, setCurrent] = useState(0)
-  const [count, setCount] = useState(post?.post.images.length)
+  const [_count, setCount] = useState(post?.post.images.length)
   const { mutate: toggleSavePostMutata } = useToggleSavePost(postId)
   const { mutate: toggleLikeMutate } = useToggleLike(postId)
+  const [editPostModalOpen, setEditPostModalOpen] = useState(false)
 
   const onBookmarkAction = () => {
     if (!post?.saved) {
@@ -67,68 +71,157 @@ export const PostDetailsModal = ({ postId }: IProp) => {
   }, [api])
 
   return (
-    <div className="flex flex-col md:flex-row md:gap-8 h-full">
-      <div className="flex-[40%] flex flex-col h-full w-full gap-4">
-        <div className="flex justify-between px-4">
-          {/* avatar in mobile */}
-          <div className="md:hidden flex items-center gap-4">
-            <Avatar className="w-[48px] h-[48px]">
-              <AvatarImage src={post?.user.imagePath} alt="user" />
-              <AvatarFallback className="bg-geryLight">
-                <UserRound color="#A5A5A5" strokeWidth={1.5} />
-              </AvatarFallback>
-            </Avatar>
-            {/* fullName in mobile */}
-            <p className="font-bold text-secondary">{post?.user.username}</p>
+    <>
+      <div className="flex flex-col md:flex-row md:gap-8 h-full">
+        <div className="flex-[40%] flex flex-col h-full w-full gap-4">
+          <div className="md:hidden flex justify-between px-4">
+            {/* avatar in mobile */}
+            <div className="md:hidden flex items-center gap-4">
+              <Avatar className="w-[48px] h-[48px]">
+                <AvatarImage src={post?.user.imagePath} alt="user" />
+                <AvatarFallback className="bg-geryLight">
+                  <UserRound color="#A5A5A5" strokeWidth={1.5} />
+                </AvatarFallback>
+              </Avatar>
+              {/* fullName in mobile */}
+              <p className="font-bold text-secondary">{post?.user.username}</p>
+            </div>
+            {/* eidt btn in mobile */}
+            <Button
+              className="md:hidden min-w-min"
+              variant="link"
+              onClick={() => setEditPostModalOpen(true)}
+            >
+              <Pencil />
+            </Button>
           </div>
-          {/* eidt btn in mobile */}
-          <Button className="md:hidden min-w-min" variant="link">
-            <Pencil />
-          </Button>
-        </div>
-        <Carousel
-          setApi={setApi}
-          className="md:w-[520px] h-auto static flex justify-center items-center flex-col"
-          style={{ direction: 'ltr' }}
-        >
-          <CarouselContent>
-            {post?.post.images.map((image, index) => (
-              <CarouselItem key={index}>
-                <img
-                  className="max-md:h-[350px] max-md:w-full md:rounded-3xl object-cover"
-                  src={image.url}
+          <Carousel
+            setApi={setApi}
+            className="w-full md:w-[520px] md:h-full static flex justify-center items-center flex-col !pl-0"
+            style={{ direction: 'ltr' }}
+          >
+            <CarouselContent className="w-full md:w-[520px]">
+              {post?.post.images.map((image, index) => (
+                <CarouselItem
+                  className="w-full flex justify-center items-center"
+                  key={index}
+                >
+                  <img
+                    className="h-[375px] md:rounded-3xl md:w-[500px] md:h-[550px] object-contain"
+                    src={image.url}
+                  />
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <div className="flex gap-2">
+              <CarouselPrevious
+                variant="secondary"
+                className="static p-0 rounded-full min-w-fit w-6 h-6 shadow border-geryLight"
+              />
+              <CarouselNext
+                variant="secondary"
+                className="static p-0 rounded-full min-w-fit w-6 h-6 shadow border border-geryLight"
+              />
+            </div>
+            <div className="max-md:hidden text-muted-foreground py-2 text-xs">
+              تصویر {post?.post.images.length} از {current}
+            </div>
+            {/* post actions(like, bookmark, comment) in mobile */}
+            <div className="md:hidden w-full flex justify-end flex-row-reverse gap-4 px-4">
+              <div className="flex flex-col gap-2  justify-center items-center text-primary cursor-pointer w-6 h-12">
+                <MessageCircle color="#ea5a69" />
+                <span>۱۵</span>
+              </div>
+              <div
+                className="flex flex-col gap-2 justify-center items-center text-primary cursor-pointer w-6 h-12"
+                onClick={onLikeAction}
+              >
+                <Heart
+                  color="#ea5a69"
+                  fill={post?.liked ? '#ea5a69' : 'white'}
                 />
-              </CarouselItem>
-            ))}
-          </CarouselContent>
+                <span>{post?.likeCount}</span>
+              </div>
+              <div
+                className="flex flex-col gap-2 justify-center items-center text-primary cursor-pointer w-6 h-12"
+                onClick={onBookmarkAction}
+              >
+                <Bookmark
+                  color="#ea5a69"
+                  fill={post?.saved ? '#ea5a69' : 'white'}
+                />
+                <span>{post?.saveCount}</span>
+              </div>
+            </div>
+          </Carousel>
+        </div>
+        <div className="flex md:flex-[60%] flex-col h-full w-full gap-4 max-md:px-4">
+          <div className="max-md:hidden flex justify-between items-center">
+            {/* Avatar in desktop */}
+            <div className="flex items-center gap-4">
+              <Avatar className="w-[48px] h-[48px]">
+                <AvatarImage src={post?.user.imagePath} alt="user" />
+                <AvatarFallback className="bg-geryLight">
+                  <UserRound color="#A5A5A5" strokeWidth={1.5} />
+                </AvatarFallback>
+              </Avatar>
+              {/* fullName in desktop */}
+              <p className="font-bold text-secondary">{post?.user.username}</p>
+            </div>
+            {/* Eidt btn in desktop ratio */}
+            <Button onClick={() => setEditPostModalOpen(true)}>
+              <Pencil />
+              <span>ویرایش پست</span>
+            </Button>
+          </div>
+          {/* createdAt */}
+          <div className="text-xs text-gray-500">
+            <span>{dayjs(post?.post.createdAt).fromNow()}</span>
+          </div>
+          {/* caption of post */}
+          <div className="h-[100px] md:h-[190px] overflow-y-auto md:px-2">
+            {post?.post.caption ? (
+              <div
+                className="text-justify text-sm/6"
+                dangerouslySetInnerHTML={{
+                  __html: parseCaption(post?.post.caption),
+                }}
+              ></div>
+            ) : (
+              <p>کپشن ندارد</p>
+            )}
+          </div>
+          {/* mentions */}
           <div className="flex gap-2">
-            <CarouselPrevious
-              variant="secondary"
-              className="static p-0 rounded-full min-w-fit w-6 h-6 shadow border-geryLight"
-            />
-            <CarouselNext
-              variant="secondary"
-              className="static p-0 rounded-full min-w-fit w-6 h-6 shadow border border-geryLight"
-            />
+            {post?.mentionedUsernames?.length
+              ? post.mentionedUsernames.map((user, i) => (
+                  <div key={i} className="text-light">
+                    <Link to={`/profile/${user}`}>
+                      <Badge
+                        className="text-sm hover:scale-103 transition-all"
+                        variant="secondary"
+                      >
+                        {user}
+                      </Badge>
+                    </Link>
+                  </div>
+                ))
+              : null}
           </div>
-          <div className="max-md:hidden text-muted-foreground py-2 text-xs">
-            تصویر {current} از {count}
-          </div>
-          {/* post actions(like, bookmark, comment) in mobile */}
-          <div className="md:hidden w-full flex justify-end flex-row-reverse gap-4 px-4">
-            <div className="flex flex-col gap-2  justify-center items-center text-primary cursor-pointer w-6 h-12">
+          <div className="max-md:hidden flex justify-end gap-4">
+            <div className="flex flex-col gap-2 justify-center items-center text-primary cursor-pointer">
               <MessageCircle color="#ea5a69" />
               <span>۱۵</span>
             </div>
             <div
-              className="flex flex-col gap-2 justify-center items-center text-primary cursor-pointer w-6 h-12"
+              className="flex flex-col gap-2 justify-center items-center text-primary cursor-pointer"
               onClick={onLikeAction}
             >
               <Heart color="#ea5a69" fill={post?.liked ? '#ea5a69' : 'white'} />
               <span>{post?.likeCount}</span>
             </div>
             <div
-              className="flex flex-col gap-2 justify-center items-center text-primary cursor-pointer w-6 h-12"
+              className="flex flex-col gap-2 justify-center items-center text-primary cursor-pointer"
               onClick={onBookmarkAction}
             >
               <Bookmark
@@ -138,80 +231,22 @@ export const PostDetailsModal = ({ postId }: IProp) => {
               <span>{post?.saveCount}</span>
             </div>
           </div>
-        </Carousel>
-      </div>
-      <div className="flex md:flex-[60%] flex-col h-full w-full gap-4 max-md:px-4">
-        <div className="max-md:hidden flex justify-between items-center">
-          {/* Avatar in desktop */}
-          <div className="flex items-center gap-4">
-            <Avatar className="w-[48px] h-[48px]">
-              <AvatarImage src={post?.user.imagePath} alt="user" />
-              <AvatarFallback className="bg-geryLight">
-                <UserRound color="#A5A5A5" strokeWidth={1.5} />
-              </AvatarFallback>
-            </Avatar>
-            {/* fullName in desktop */}
-            <p className="font-bold text-secondary">{post?.user.username}</p>
-          </div>
-          {/* Eidt btn in desktop ratio */}
-          <Button>
-            <Pencil />
-            <span>ویرایش پست</span>
-          </Button>
-        </div>
-        {/* createdAt */}
-        <div className="text-xs text-gray-500">
-          <span>{dayjs(post?.post.createdAt).fromNow()}</span>
-        </div>
-        {/* caption of post */}
-        <div className="h-[100px] md:h-[190px] overflow-y-auto md:px-2">
-          {post?.post.caption ? (
-            <div
-              className="text-justify text-sm/6"
-              dangerouslySetInnerHTML={{
-                __html: parseCaption(post?.post.caption),
-              }}
-            ></div>
-          ) : (
-            <p>کپشن ندارد</p>
-          )}
-        </div>
-        {/* mentions */}
-        <div className="flex gap-2">
-          {post?.mentionedUsernames?.length
-            ? post.mentionedUsernames.map((user, i) => (
-                <div key={i} className="text-light">
-                  <Badge className="text-sm" variant="secondary">
-                    {user}
-                  </Badge>
-                </div>
-              ))
-            : null}
-        </div>
-        <div className="max-md:hidden flex justify-end gap-4">
-          <div className="flex flex-col gap-2 justify-center items-center text-primary cursor-pointer">
-            <MessageCircle color="#ea5a69" />
-            <span>۱۵</span>
-          </div>
-          <div
-            className="flex flex-col gap-2 justify-center items-center text-primary cursor-pointer"
-            onClick={onLikeAction}
-          >
-            <Heart color="#ea5a69" fill={post?.liked ? '#ea5a69' : 'white'} />
-            <span>{post?.likeCount}</span>
-          </div>
-          <div
-            className="flex flex-col gap-2 justify-center items-center text-primary cursor-pointer"
-            onClick={onBookmarkAction}
-          >
-            <Bookmark
-              color="#ea5a69"
-              fill={post?.saved ? '#ea5a69' : 'white'}
-            />
-            <span>{post?.saveCount}</span>
-          </div>
         </div>
       </div>
-    </div>
+      {editPostModalOpen && (
+        <DialogAndModalWizard
+          open={editPostModalOpen}
+          setOpen={setEditPostModalOpen}
+          className="h-fit flex flex-col"
+        >
+          <UploadPostForm
+            initialData={data}
+            postId={postId}
+            mode="edit"
+            onSuccess={() => setEditPostModalOpen(false)}
+          />
+        </DialogAndModalWizard>
+      )}
+    </>
   )
 }
