a1861b2c5342372cfa8245912d1450408d7cb247
mmreza <mmrezadev@gmail.com>
2025-09-14 21:17:25 +0330

    modify :: redircet user to login after success register

--- FrontEnd/src/features/auth/components/RegisterForm.tsx
+++ FrontEnd/src/features/auth/components/RegisterForm.tsx
@@ -15,12 +15,16 @@ import {
   userNameSchema,
   validateWithYup,
 } from '@/utils/validation'
-import { type ComponentProps } from 'react'
+import { useEffect, type ComponentProps } from 'react'
 import { useRegister } from '../hooks/useAuth'
 import { ValidationError } from 'yup'
 
-export const RegisterForm = () => {
-  const { mutate: registerMutate, isPending } = useRegister()
+interface IProps {
+  onSuccess?: () => void
+}
+
+export const RegisterForm = ({ onSuccess }: IProps) => {
+  const { mutate: registerMutate, isPending, isSuccess } = useRegister()
   const userName = useInput('userName', '', (val) =>
     validateWithYup(userNameSchema, val),
   )
@@ -35,6 +39,10 @@ export const RegisterForm = () => {
       context: { password: password.value },
     }),
   )
+
+  useEffect(() => {
+    if (isSuccess) onSuccess?.()
+  }, [isSuccess])
 
   const onFormSubmit: ComponentProps<'form'>['onSubmit'] = (e) => {
     e.preventDefault()
