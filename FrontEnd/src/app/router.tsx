3137fb154b564384d40b45590fd62d00194e8e41
kianoosh <kianoosh.koochaki@gmail.com>
2025-09-23 03:17:54 +0330

    Error in SEARCH PAGE

--- FrontEnd/src/app/router.tsx
+++ FrontEnd/src/app/router.tsx
@@ -9,11 +9,7 @@ import { Tagged } from '@/features/tagged/pages/Tagged'
 import { Saves } from '@/features/saved/pages/Saves'
 import { PostDetailsPage } from '@/features/post/pages/PostDetailsPage'
 import { PostLayout } from '@/layouts/PostLayout'
-<<<<<<< HEAD
 import { SearchPage } from '@/features/search/pages/search'
-=======
-import { SearchPage } from '@/features/search/pages/searchpage'
->>>>>>> 60d25acf0c6f54b9d6034c63ee62d7108fa7d600
 
 function ProtectedRoute() {
   const { data: me, isLoading } = useMe()
