450305c471cb6a74e9d983d19327e089fccf067a
kianoosh <kianoosh.koochaki@gmail.com>
2025-09-26 08:06:24 +0330

    morepage now support tabbing officialy

--- FrontEnd/src/features/more/pages/morepage.tsx
+++ FrontEnd/src/features/more/pages/morepage.tsx
@@ -3,14 +3,21 @@ import MoreTabsBar from '../components/MoreTabs'
 import CloseFriendsPage from '../components/closefriends'
 import BlockListPage from '../components/blocklist'
 
-export const MorePage = () => {
-  const [activeTab, setActiveTab] = useState<'closefriends' | 'blocklist'>(
-    'closefriends',
-  )
+interface MorePageProps {
+  activeTab?: 'closefriends' | 'blocklist'
+  setActiveTab?: (_: 'closefriends' | 'blocklist') => void
+}
 
+export const MorePage = ({
+  activeTab = 'closefriends',
+  setActiveTab,
+}: MorePageProps) => {
   return (
     <>
-      <MoreTabsBar activeTab={activeTab} setActiveTab={setActiveTab} />
+      <MoreTabsBar
+        activeTab={activeTab}
+        setActiveTab={setActiveTab ?? (() => {})}
+      />
       {/* Render tab content based on activeTab */}
       {activeTab === 'closefriends' && <CloseFriendsPage />}
       {activeTab === 'blocklist' && <BlockListPage />}
